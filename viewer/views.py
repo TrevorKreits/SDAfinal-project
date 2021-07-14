fcdd9d196156421ccf209a2840156f529cf24426
[Trevor Kreitsberg] <[trevor.kreitsberg@outlook.com]>
2021-07-14 13:13:45 +0300

    creating first page

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-# Create your views here.
+def first(response):
+    return HttpResponse('Hello!')
+
