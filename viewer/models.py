9f3144d331d5bb7a307231ce483d7c72a037e530
[Trevor Kreitsberg] <[trevor.kreitsberg@outlook.com]>
2021-09-02 10:11:43 +0300

    initaial.py commit

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Services(models.Model):
+    service = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.service
+
+class Providers(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    country = models.CharField(max_length=50, null=True, blank=True)
+    gender = models.CharField(max_length=20, null=True, blank=True)
+    date_birth = models.DateField(null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
