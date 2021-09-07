b52d59c6e3da3bef9a028171546fe78f5da90f6a
[Trevor Kreitsberg] <[trevor.kreitsberg@outlook.com]>
2021-09-07 20:16:18 +0300

    Service and categories models

--- viewer/models.py
+++ viewer/models.py
@@ -1,17 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Services(models.Model):
-    service = models.CharField(max_length=50)
-    description = models.TextField(null=True, blank=True)
+
+class Categories(models.Model):
+    name = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.service
+        return self.name
+
+
+class Services(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    categories = models.ForeignKey(Categories, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Providers(models.Model):
     name = models.CharField(max_length=50)
+    services = models.ForeignKey(Services, on_delete=models.DO_NOTHING, null=True, blank=True)
+
     def __str__(self):
         return self.name
+
 
 class Profile(models.Model):
     country = models.CharField(max_length=50, null=True, blank=True)
